b5eee4cb316055fbef85eee4c28e75197687398c
sergey <serge.dmitriev@gmail.com>
2015-09-05 22:28:01 +0700

    add socket.io load from asti server

--- js/index.js
+++ js/index.js
@@ -3,9 +3,51 @@
 
 var AMI = function (url) {
 
+  var socket;
+
+  var addScript = function (callback) {
+    var script = document.createElement('script');
+    script.setAttribute('src', url + '/socket.io/socket.io.js');
+    script.onload = callback;
+    document.body.appendChild(script);
+  };
+
+  var checkSocket = function () {
+    if (!socket) {
+      throw new Error('No socket.io conection');
+    };
+  };
+
+  var connect = function () {
+    addScript(function(){
+      socket = io(url);
+    });
+  };
+
+  var subscribe = function (object) {
+    checkSocket();
+    socket.emit('subscribe', object);
+
+    var handler = function (data) {
+      console.log(data);
+    };
+
+    socket.on('agentcalled', handler);
+    socket.on('agentconnect', handler);
+    socket.on('agentcomplete', handler);
+    socket.on('agentdump', handler);
+    socket.on('agentringnoanswer', handler);
+  };
+
+  var unsibscribe = function (object) {
+    checkSocket();
+    socket.emit('unsibscribe', object);
+  };
+
   var call = function (channel, context, exten, variable) {
     var r = [
       url,
+      '/call',
       '?',
       'channel=' + channel,
       '&',
@@ -16,9 +58,13 @@ var AMI = function (url) {
       'variable=' + variable
     ].join('');
     return fetch(r);
-  }
+  };
 
   return {
-    call: call
+    call: call,
+    connect: connect,
+    subscribe: subscribe,
+    unsibscribe: unsibscribe
   };
+
 };
